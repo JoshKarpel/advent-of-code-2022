45999a7132d2bda04f23102649712d81438a81b1
Josh Karpel <josh.karpel@gmail.com>
2022-11-30 23:21:28 -0600

    first pass

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,13 +1,39 @@
 defmodule Day01 do
   def solve do
-    [part1(), part2()]
+    chunk_fun = fn e, acc ->
+      case e do
+        "" -> {:cont, acc, []}
+        e -> {:cont, [e | acc]}
+      end
+    end
+
+    after_fun = fn
+      [] -> {:cont, []}
+      acc -> {:cont, Enum.reverse(acc), []}
+    end
+
+    data =
+      File.stream!("data/day_01.txt")
+      |> Enum.map(&String.trim/1)
+      |> Enum.chunk_while([], chunk_fun, after_fun)
+      |> Enum.map(fn nums -> Enum.map(nums, &String.to_integer/1) end)
+
+    IO.inspect(data)
+
+    [part1(data), part2(data)]
   end
 
-  def part1 do
-    0
+  def part1(data) do
+    data |> Enum.map(&Enum.sum/1) |> Enum.max() |> IO.inspect()
   end
 
-  def part2 do
-    0
+  def part2(data) do
+    data
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort()
+    |> Enum.reverse()
+    |> Enum.take(3)
+    |> Enum.sum()
+    |> IO.inspect()
   end
 end
