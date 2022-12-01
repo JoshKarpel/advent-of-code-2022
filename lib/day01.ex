8c2f369d7ec4b6088db82a36967319d613e757ca
Josh Karpel <josh.karpel@gmail.com>
2022-12-01 00:08:06 -0600

    use flat_map to hack together filter_map

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,18 +1,18 @@
 defmodule Day01 do
   def solve do
-    ordered_calorie_counts =
+    descending_calorie_counts =
       File.stream!("data/day_01.txt")
       |> Enum.map(&String.trim/1)
       |> Enum.chunk_by(&(&1 == ""))
-      |> Enum.reject(&(&1 == [""]))
-      |> Enum.map(fn nums -> Enum.map(nums, &String.to_integer/1) end)
-      |> Enum.map(&Enum.sum/1)
-      |> Enum.sort()
-      |> Enum.reverse()
+      |> Enum.flat_map(fn
+        [""] -> []
+        cals -> [cals |> Enum.map(&String.to_integer/1) |> Enum.sum()]
+      end)
+      |> Enum.sort_by(& &1, :desc)
 
     [
-      ordered_calorie_counts |> Enum.max(),
-      ordered_calorie_counts
+      descending_calorie_counts |> Enum.at(0),
+      descending_calorie_counts
       |> Enum.take(3)
       |> Enum.sum()
     ]
