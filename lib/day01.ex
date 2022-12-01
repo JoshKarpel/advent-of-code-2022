c8b53759735ec2d1bb99e1b02b1c516ce8a91297
Josh Karpel <josh.karpel@gmail.com>
2022-11-30 23:39:02 -0600

    tidy up

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,39 +1,20 @@
 defmodule Day01 do
   def solve do
-    chunk_fun = fn e, acc ->
-      case e do
-        "" -> {:cont, acc, []}
-        e -> {:cont, [e | acc]}
-      end
-    end
-
-    after_fun = fn
-      [] -> {:cont, []}
-      acc -> {:cont, Enum.reverse(acc), []}
-    end
-
-    data =
+    ordered_calorie_counts =
       File.stream!("data/day_01.txt")
       |> Enum.map(&String.trim/1)
-      |> Enum.chunk_while([], chunk_fun, after_fun)
+      |> Enum.chunk_by(&(&1 == ""))
+      |> Enum.reject(&(&1 == [""]))
       |> Enum.map(fn nums -> Enum.map(nums, &String.to_integer/1) end)
+      |> Enum.map(&Enum.sum/1)
+      |> Enum.sort()
+      |> Enum.reverse()
 
-    IO.inspect(data)
-
-    [part1(data), part2(data)]
-  end
-
-  def part1(data) do
-    data |> Enum.map(&Enum.sum/1) |> Enum.max() |> IO.inspect()
-  end
-
-  def part2(data) do
-    data
-    |> Enum.map(&Enum.sum/1)
-    |> Enum.sort()
-    |> Enum.reverse()
-    |> Enum.take(3)
-    |> Enum.sum()
-    |> IO.inspect()
+    [
+      ordered_calorie_counts |> Enum.max(),
+      ordered_calorie_counts
+      |> Enum.take(3)
+      |> Enum.sum()
+    ]
   end
 end
