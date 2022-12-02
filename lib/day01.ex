bfed5655db63466f693d19125525ec10e9a97408
Josh Karpel <josh.karpel@gmail.com>
2022-12-01 20:39:56 -0600

    pipes

--- lib/day01.ex
+++ lib/day01.ex
@@ -1,20 +1,13 @@
 defmodule Day01 do
   def solve do
-    descending_calorie_counts =
-      File.stream!("data/day_01.txt")
-      |> Enum.map(&String.trim/1)
-      |> Enum.chunk_by(&(&1 == ""))
-      |> Enum.flat_map(fn
-        [""] -> []
-        cals -> [cals |> Enum.map(&String.to_integer/1) |> Enum.sum()]
-      end)
-      |> Enum.sort_by(& &1, :desc)
-
-    [
-      descending_calorie_counts |> Enum.at(0),
-      descending_calorie_counts
-      |> Enum.take(3)
-      |> Enum.sum()
-    ]
+    File.stream!("data/day_01.txt")
+    |> Enum.map(&String.trim/1)
+    |> Enum.chunk_by(&(&1 == ""))
+    |> Enum.flat_map(fn
+      [""] -> []
+      cals -> [cals |> Enum.map(&String.to_integer/1) |> Enum.sum()]
+    end)
+    |> Enum.sort_by(& &1, :desc)
+    |> then(&[&1 |> Enum.at(0), &1 |> Enum.take(3) |> Enum.sum()])
   end
 end
