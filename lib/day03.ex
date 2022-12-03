9a89fd2eefe83fc25891d671f406e6679024ab7b
Josh Karpel <josh.karpel@gmail.com>
2022-12-03 00:23:02 -0600

    solve part 2

--- lib/day03.ex
+++ lib/day03.ex
@@ -1,21 +1,31 @@
 defmodule Day03 do
   def solve do
-    rounds =
+    rucksacks =
       File.stream!("data/day_03.txt")
       |> Enum.map(&String.trim(&1))
+
+    priority_sum =
+      rucksacks
       |> Enum.map(&halves/1)
-      |> IO.inspect()
       |> Enum.map(fn {l, r} ->
         MapSet.intersection(MapSet.new(to_charlist(l)), MapSet.new(to_charlist(r)))
       end)
-      |> IO.inspect()
       |> Enum.map(&List.first(MapSet.to_list(&1)))
-      |> IO.inspect()
       |> Enum.map(&priority/1)
-      |> IO.inspect()
       |> Enum.sum()
 
-    {rounds, 0}
+    badge_sum =
+      rucksacks
+      |> Enum.chunk_every(3)
+      |> IO.inspect()
+      |> Enum.map(fn sacks -> Enum.map(sacks, &MapSet.new(to_charlist(&1))) end)
+      |> IO.inspect()
+      |> Enum.map(fn sacks -> Enum.reduce(sacks, &MapSet.intersection/2) end)
+      |> Enum.map(&List.first(MapSet.to_list(&1)))
+      |> Enum.map(&priority/1)
+      |> Enum.sum()
+
+    {priority_sum, badge_sum}
   end
 
   def halves(s) do
