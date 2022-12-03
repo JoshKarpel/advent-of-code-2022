9f682b6c10afc31b41eef98dbd62b4c3be3fb967
Josh Karpel <josh.karpel@gmail.com>
2022-12-03 00:16:13 -0600

    part 1

--- lib/day03.ex
+++ lib/day03.ex
@@ -1,11 +1,34 @@
 defmodule Day03 do
   def solve do
     rounds =
-      File.stream!("data/day_02.txt")
+      File.stream!("data/day_03.txt")
       |> Enum.map(&String.trim(&1))
-      |> Enum.map(&String.split(&1, " "))
-      |> Enum.map(&List.to_tuple(&1))
+      |> Enum.map(&halves/1)
+      |> IO.inspect()
+      |> Enum.map(fn {l, r} ->
+        MapSet.intersection(MapSet.new(to_charlist(l)), MapSet.new(to_charlist(r)))
+      end)
+      |> IO.inspect()
+      |> Enum.map(&List.first(MapSet.to_list(&1)))
+      |> IO.inspect()
+      |> Enum.map(&priority/1)
+      |> IO.inspect()
+      |> Enum.sum()
 
-    {0, 0}
+    {rounds, 0}
+  end
+
+  def halves(s) do
+    String.split_at(s, div(String.length(s), 2))
+  end
+
+  def priority(c) do
+    case c do
+      c when c < 95 ->
+        c - 65 + 27
+
+      c when c >= 95 ->
+        c - 97 + 1
+    end
   end
 end
