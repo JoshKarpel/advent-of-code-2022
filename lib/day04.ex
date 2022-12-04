165f17fdd274c2934d739afa63a536270fc5d9b2
Josh Karpel <josh.karpel@gmail.com>
2022-12-04 11:09:46 -0600

    simplify full_overlap

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,22 +1,21 @@
 defmodule Day04 do
   def solve do
-    full_overlaps =
+    pairs =
       File.stream!("data/day_04.txt")
       |> Enum.map(&String.trim(&1))
       |> Enum.map(&Regex.run(~r/(\d+)\-(\d+),(\d+)\-(\d+)/, &1, capture: :all_but_first))
       |> Enum.map(fn nums -> Enum.map(nums, &String.to_integer/1) end)
-      |> IO.inspect()
       |> Enum.map(fn [a, b, c, d] -> {a..b, c..d} end)
-      |> IO.inspect()
-      |> Enum.filter(fn {r1, r2} ->
-        case {r1, r2} do
-          {r1, r2} when r1.first >= r2.first and r1.last <= r2.last -> true
-          {r1, r2} when r2.first >= r1.first and r2.last <= r1.last -> true
-          _ -> false
-        end
-      end)
-      |> Enum.count()
 
-    {full_overlaps, 0}
+    {pairs
+     |> Enum.filter(fn {a, b} -> full_overlap?(a, b) end)
+     |> Enum.count(),
+     pairs
+     |> Enum.reject(fn {a, b} -> Range.disjoint?(a, b) end)
+     |> Enum.count()}
+  end
+
+  def full_overlap?(r1, r2) do
+    (r1.first >= r2.first and r1.last <= r2.last) or (r2.first >= r1.first and r2.last <= r1.last)
   end
 end
