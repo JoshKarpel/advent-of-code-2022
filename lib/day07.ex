1e6d7f08d859a46aa8e4839c323118657ce95501
Josh Karpel <josh.karpel@gmail.com>
2022-12-07 00:08:24 -0600

    solve part 2, clean up

--- lib/day07.ex
+++ lib/day07.ex
@@ -1,23 +1,31 @@
 defmodule Day07 do
   @total 70_000_000
+  @desired_unused 30_000_000
 
   def solve do
-    dir_sizes =
+    dir_sizes_by_path =
       File.stream!("data/day_07.txt")
       |> Enum.map(&String.trim_trailing/1)
-      |> Enum.map(&String.split/1)
       |> Enum.map(&parse_line/1)
       |> execute
-      |> IO.inspect()
+
+    used = dir_sizes_by_path |> Map.get(["/"])
+    min_to_delete = @desired_unused - (@total - used)
+
+    dir_sizes =
+      dir_sizes_by_path
+      |> Map.values()
 
     {dir_sizes
-     |> Map.values()
      |> Enum.filter(&(&1 <= 100_000))
-     |> Enum.sum(), 0}
+     |> Enum.sum(),
+     dir_sizes
+     |> Enum.filter(&(&1 >= min_to_delete))
+     |> Enum.min()}
   end
 
   def parse_line(line) do
-    case line do
+    case line |> String.split() do
       ["$", "cd", ".."] ->
         {:cd, :up}
 
@@ -36,7 +44,7 @@ defmodule Day07 do
   end
 
   def execute(instructions) do
-    {cwd, dir_sizes} =
+    {_, dir_sizes} =
       instructions
       |> Enum.reduce({[], %{}}, fn instruction, {path, dir_sizes} ->
         case instruction do
@@ -49,19 +57,16 @@ defmodule Day07 do
               dir_sizes
             }
 
-          {:ls} ->
-            {path, dir_sizes}
-
-          {:dir, name} ->
-            {path, dir_sizes}
-
-          {:file, name, size} ->
+          {:file, _name, size} ->
             path
             |> Enum.reverse()
             |> Enum.reduce({[], dir_sizes}, fn p, {path, ds} ->
               {[p | path],
                Map.update(ds, [p | path], size, fn existing_value -> existing_value + size end)}
             end)
+
+          _ ->
+            {path, dir_sizes}
         end
       end)
 
