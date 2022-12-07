60050346e0060cb174be172979a225e3b67c2f5f
Josh Karpel <josh.karpel@gmail.com>
2022-12-07 09:34:46 -0600

    Refactor Day 7 (#12)

--- lib/day07.ex
+++ lib/day07.ex
@@ -6,7 +6,6 @@ defmodule Day07 do
     dir_sizes_by_path =
       File.stream!("data/day_07.txt")
       |> Enum.map(&String.trim_trailing/1)
-      |> Enum.map(&parse_line/1)
       |> execute
 
     used = dir_sizes_by_path |> Map.get(["/"])
@@ -24,45 +23,34 @@ defmodule Day07 do
      |> Enum.min()}
   end
 
-  def parse_line(line) do
-    case line |> String.split() do
-      ["$", "cd", ".."] ->
-        {:cd, :up}
-
-      ["$", "cd", target] ->
-        {:cd, target}
-
-      ["$", "ls"] ->
-        {:ls}
-
-      ["dir", name] ->
-        {:dir, name}
-
-      [size, name] ->
-        {:file, name, String.to_integer(size)}
-    end
-  end
-
   def execute(instructions) do
     {_, dir_sizes} =
       instructions
       |> Enum.reduce({[], %{}}, fn instruction, {path, dir_sizes} ->
-        case instruction do
-          {:cd, :up} ->
+        case instruction |> String.split() do
+          ["$", "cd", ".."] ->
             {List.delete_at(path, 0), dir_sizes}
 
-          {:cd, target} ->
+          ["$", "cd", target] ->
             {
               [target | path],
               dir_sizes
             }
 
-          {:file, _name, size} ->
+          ["$", "ls"] ->
+            {path, dir_sizes}
+
+          ["dir", _name] ->
+            {path, dir_sizes}
+
+          [size, _name] ->
             path
             |> Enum.reverse()
             |> Enum.reduce({[], dir_sizes}, fn p, {path, ds} ->
-              {[p | path],
-               Map.update(ds, [p | path], size, fn existing_value -> existing_value + size end)}
+              new_path = [p | path]
+              s = String.to_integer(size)
+
+              {new_path, Map.update(ds, new_path, s, &(&1 + s))}
             end)
 
           _ ->
