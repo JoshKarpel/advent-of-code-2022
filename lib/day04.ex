da64f9c9604400d17933c664e1e174d66b9a733e
Josh Karpel <josh.karpel@gmail.com>
2022-12-07 09:33:29 -0600

    fix day 4

--- lib/day04.ex
+++ lib/day04.ex
@@ -1,19 +1,18 @@
 defmodule Day04 do
   def solve do
-    pairs =
-      File.stream!("data/day_04.txt")
-      |> Enum.map(&String.trim(&1))
-      |> Enum.map(&Regex.run(~r/(\d+)\-(\d+),(\d+)\-(\d+)/, &1, capture: :all_but_first))
-      |> Enum.map(fn nums -> Enum.map(nums, &String.to_integer/1) end)
-      |> Enum.map(fn [a, b, c, d] -> {a..b, c..d} end)
-      |> then(
-        &{&1
-         |> Enum.filter(fn {a, b} -> fully_contained?(a, b) or fully_contained?(b, a) end)
-         |> Enum.count(),
-         &1
-         |> Enum.reject(fn {a, b} -> Range.disjoint?(a, b) end)
-         |> Enum.count()}
-      )
+    File.stream!("data/day_04.txt")
+    |> Enum.map(&String.trim(&1))
+    |> Enum.map(&Regex.run(~r/(\d+)\-(\d+),(\d+)\-(\d+)/, &1, capture: :all_but_first))
+    |> Enum.map(fn nums -> Enum.map(nums, &String.to_integer/1) end)
+    |> Enum.map(fn [a, b, c, d] -> {a..b, c..d} end)
+    |> then(
+      &{&1
+       |> Enum.filter(fn {a, b} -> fully_contained?(a, b) or fully_contained?(b, a) end)
+       |> Enum.count(),
+       &1
+       |> Enum.reject(fn {a, b} -> Range.disjoint?(a, b) end)
+       |> Enum.count()}
+    )
   end
 
   def fully_contained?(a, b) do
