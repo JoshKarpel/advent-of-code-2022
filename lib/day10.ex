d6c13d4eac54836279dee68881d90a970ba7d5f5
Josh Karpel <josh.karpel@gmail.com>
2022-12-09 23:47:42 -0600

    solve part 2

--- lib/day10.ex
+++ lib/day10.ex
@@ -1,10 +1,10 @@
 defmodule Day10 do
   def solve do
-    {_, _, signal} =
+    {_, _, signal, lit_pixels} =
       File.stream!("data/day_10.txt")
       |> Enum.map(&String.trim_trailing/1)
-      |> Enum.reduce({0, 1, 0}, fn
-        instruction, {cycle, x, signal} ->
+      |> Enum.reduce({0, 1, 0, MapSet.new()}, fn
+        instruction, {cycle, x, signal, lit_pixels} ->
           {new_cycle, new_x} =
             case instruction |> String.split(" ") do
               ["noop"] ->
@@ -17,11 +17,38 @@ defmodule Day10 do
           cycle_hit =
             cycle..new_cycle |> Enum.drop(1) |> Enum.find(0, fn c -> rem(c - 20, 40) == 0 end)
 
+          lit_pixels =
+            cycle..new_cycle
+            |> Enum.drop(1)
+            |> Enum.reduce(lit_pixels, fn c, pix ->
+              # -1 to convert from cycle to position after wrapping onto the row
+              if abs(rem(c, 40) - 1 - x) <= 1 do
+                pix |> MapSet.put(c)
+              else
+                pix
+              end
+            end)
+
           signal = signal + x * cycle_hit
 
-          {new_cycle, new_x, signal}
+          {new_cycle, new_x, signal, lit_pixels}
       end)
 
-    {signal, 0}
+    lit_pixels |> IO.inspect()
+
+    image =
+      "\n" <>
+        (1..240
+         |> Enum.map(fn c ->
+           if MapSet.member?(lit_pixels, c) do
+             "█"
+           else
+             " "
+           end
+         end)
+         |> Enum.chunk_every(40)
+         |> Enum.join("\n"))
+
+    {signal, image}
   end
 end
