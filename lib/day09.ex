29e5261b2c38e139d2479f41c05b9dd37d6064ca
Josh Karpel <josh.karpel@gmail.com>
2022-12-09 22:33:35 -0600

    refactor for part 2

--- lib/day09.ex
+++ lib/day09.ex
@@ -1,6 +1,6 @@
 defmodule Day09 do
   def solve do
-    {_, _, visited} =
+    instructions =
       File.stream!("data/day_09.txt")
       |> Enum.map(&String.trim_trailing/1)
       |> Enum.flat_map(fn instruction ->
@@ -9,44 +9,43 @@ defmodule Day09 do
 
         1..d |> Enum.map(fn _ -> dir end)
       end)
-      |> IO.inspect()
-      |> Enum.reduce({{0, 0}, {0, 0}, MapSet.new([{0, 0}])}, fn dir,
-                                                                {{h_x, h_y}, {t_x, t_y}, visited} ->
-        IO.inspect(dir)
 
-        {new_h_x, new_h_y} =
-          case dir do
-            "L" -> {h_x - 1, h_y}
-            "R" -> {h_x + 1, h_y}
-            "U" -> {h_x, h_y + 1}
-            "D" -> {h_x, h_y - 1}
-          end
+    {_, _, visited} =
+      instructions
+      |> Enum.reduce({{0, 0}, {0, 0}, MapSet.new([{0, 0}])}, fn dir, {h, t, visited} ->
+        new_h = next_head_position(dir, h)
 
-        new_t =
-          case {new_h_x - t_x, new_h_y - t_y} do
-            {x, y} when abs(x) <= 1 and abs(y) <= 1 ->
-              {t_x, t_y}
+        new_t = next_tail_position(new_h, t)
 
-            {x, 0} ->
-              {t_x + sign(x), t_y}
-
-            {0, y} ->
-              {t_x, t_y + sign(y)}
-
-            {x, y} when abs(y) == 1 ->
-              {t_x + sign(x), t_y + sign(y)}
-
-            {x, y} when abs(x) == 1 ->
-              {t_x + sign(x), t_y + sign(y)}
-          end
-
-        {{new_h_x, new_h_y}, new_t, visited |> MapSet.put(new_t)} |> IO.inspect()
+        {new_h, new_t, visited |> MapSet.put(new_t)}
       end)
 
     {visited |> MapSet.size(), 0}
   end
 
+  def next_head_position(dir, {h_x, h_y}) do
+    case dir do
+      "L" -> {h_x - 1, h_y}
+      "R" -> {h_x + 1, h_y}
+      "U" -> {h_x, h_y + 1}
+      "D" -> {h_x, h_y - 1}
+    end
+  end
+
+  def next_tail_position({new_h_x, new_h_y}, {t_x, t_y}) do
+    case {new_h_x - t_x, new_h_y - t_y} do
+      {x, y} when abs(x) <= 1 and abs(y) <= 1 ->
+        {t_x, t_y}
+
+      {x, y} ->
+        {t_x + sign(x), t_y + sign(y)}
+    end
+  end
+
   def sign(x) do
-    div(abs(x), x)
+    case x do
+      0 -> 0
+      x -> div(abs(x), x)
+    end
   end
 end
