401f1313476cc8a24f0632de3b440ce837c31a3d
Josh Karpel <josh.karpel@gmail.com>
2022-12-11 23:31:53 -0600

    edges

--- lib/day12.ex
+++ lib/day12.ex
@@ -3,7 +3,6 @@ defmodule Day12 do
     {start, target, heights} =
       File.stream!("data/day_12.txt")
       |> Enum.map(&String.trim/1)
-      |> IO.inspect()
       |> Enum.with_index()
       |> Enum.flat_map(fn {line, y} ->
         line
@@ -11,13 +10,30 @@ defmodule Day12 do
         |> Enum.with_index()
         |> Enum.map(fn {h, x} -> {{x, y}, h} end)
       end)
-      |> IO.inspect()
       |> Enum.reduce({nil, nil, %{}}, fn {{x, y}, h}, {start, target, heights} ->
         case h do
           83 -> {{x, y}, target, heights |> Map.put({x, y}, 0)}
           69 -> {start, {x, y}, heights |> Map.put({x, y}, 25)}
           h when h >= 97 -> {start, target, heights |> Map.put({x, y}, h - 97)}
         end
+      end)
+      |> IO.inspect()
+
+    edges =
+      heights
+      |> Enum.reduce(%{}, fn {{x, y}, h}, outer_acc ->
+        [{1, 0}, {-1, 0}, {0, 1}, {0, -1}]
+        |> Enum.reduce(outer_acc, fn {dx, dy}, inner_acc ->
+          test = {x + dx, y + dy}
+
+          case Map.get(heights, test) do
+            other_h when other_h <= h + 1 ->
+              inner_acc |> Map.update({x, y}, [], fn others -> [test | others] end)
+
+            _ ->
+              inner_acc
+          end
+        end)
       end)
       |> IO.inspect()
 
