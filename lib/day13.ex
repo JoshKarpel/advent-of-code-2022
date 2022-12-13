aab85d0b7baa63b0d4f025ababaf17503e32bfbc
Josh Karpel <josh.karpel@gmail.com>
2022-12-13 00:29:12 -0600

    correct on example

--- lib/day13.ex
+++ lib/day13.ex
@@ -1,9 +1,60 @@
 defmodule Day13 do
   def solve do
-    File.stream!("data/day_13.txt")
-    |> Enum.map(&String.trim/1)
-    |> IO.inspect()
+    p1 =
+      File.stream!("data/day_13.txt")
+      |> Enum.map(&String.trim/1)
+      |> Enum.chunk_by(&(&1 == ""))
+      |> Enum.reject(&(&1 == [""]))
+      |> Enum.map(fn pair ->
+        pair
+        |> Enum.map(fn packet ->
+          {result, _} = Code.eval_string(packet)
+          result
+        end)
+      end)
+      |> Enum.with_index(1)
+      |> Enum.filter(fn {lr, idx} ->
+        IO.puts("")
+        IO.puts(idx)
+        right_order?(lr)
+      end)
+      #      |> IO.inspect(charlists: :as_lists)
+      |> Enum.map(fn {_, idx} -> idx end)
+      |> Enum.sum()
 
-    {0, 0}
+    {p1, 0}
+  end
+
+  def right_order?([left, right]) do
+    IO.inspect({left, right}, charlists: :as_lists)
+
+    case {left |> List.pop_at(0), right |> List.pop_at(0)} do
+      {{nil, []}, {nil, []}} ->
+        true |> IO.inspect(label: "both empty")
+
+      {{nil, []}, _} ->
+        true |> IO.inspect(label: "left ran out")
+
+      {_, {nil, []}} ->
+        false |> IO.inspect(label: "right ran out")
+
+      {{l, rest_l}, {r, rest_r}} when is_integer(l) and is_integer(r) and l > r ->
+        false |> IO.inspect(label: "wrong sorted ints")
+
+      {{l, rest_l}, {r, rest_r}} when is_integer(l) and is_integer(r) and l < r ->
+        true |> IO.inspect(label: "right sorted ints")
+
+      {{l, rest_l}, {r, rest_r}} when is_list(l) and is_list(r) ->
+        right_order?([l, r]) and right_order?([rest_l, rest_r])
+
+      {{l, rest_l}, {r, rest_r}} when is_integer(l) and is_list(r) ->
+        right_order?([[l], r]) and right_order?([rest_l, rest_r])
+
+      {{l, rest_l}, {r, rest_r}} when is_list(l) and is_integer(r) ->
+        right_order?([l, [r]]) and right_order?([rest_l, rest_r])
+
+      {{_, rest_l}, {_, rest_r}} ->
+        right_order?([rest_l, rest_r])
+    end
   end
 end
