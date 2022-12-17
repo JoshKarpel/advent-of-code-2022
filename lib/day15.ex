39919415cbd8bec0f918412b5303a31d995eeb17
Josh Karpel <josh.karpel@gmail.com>
2022-12-17 02:12:46 -0600

    tidy up

--- lib/day15.ex
+++ lib/day15.ex
@@ -1,8 +1,8 @@
 defmodule Day15 do
-  @y 10
+  @y 2_000_000
 
   @lower_bound 0
-  @upper_bound 20
+  @upper_bound 4_000_000
 
   @multiplier 4_000_000
 
@@ -17,38 +17,25 @@ defmodule Day15 do
           capture: :all_but_first
         )
         |> Enum.map(&String.to_integer/1)
-        |> then(fn [s_x, s_y, b_x, b_y] -> {{s_x, s_y}, {b_x, b_y}} end)
+        |> then(fn [s_x, s_y, b_x, b_y] ->
+          {{s_x, s_y}, {b_x, b_y}, manhattan_distance({s_x, s_y}, {b_x, b_y})}
+        end)
       end)
-
-    sensor_to_distance =
-      sensors_and_beacons
-      |> Enum.map(fn {s, b} -> {s, manhattan_distance(s, b)} end)
-      |> Map.new()
-
-    sensors = sensors_and_beacons |> Enum.map(fn {s, _} -> s end) |> MapSet.new()
-
-    beacons =
-      sensors_and_beacons
-      |> Enum.map(fn {_, b} -> b end)
-      |> MapSet.new()
-      |> Enum.count()
 
     p1 =
       (sensors_and_beacons
        |> outlawed_on_fixed_y_line(@y)
-       |> IO.inspect()
        |> Enum.map(&Enum.count(&1))
        |> Enum.sum()) - count_beacons_at_fixed_y(sensors_and_beacons, @y)
 
     p2 =
       @lower_bound..@upper_bound
-      |> Enum.map(fn y ->
-        IO.puts("\ny=#{y}")
-        {y, outlawed_on_fixed_y_line(sensors_and_beacons, y)} |> IO.inspect()
+      |> Stream.map(fn y ->
+        {y, outlawed_on_fixed_y_line(sensors_and_beacons, y)}
       end)
       |> Enum.find_value(fn
-        {y, [a]} -> false
-        {y, [a, b]} -> {a.last + 1, y} |> IO.inspect()
+        {y, [a, _]} -> {a.last + 1, y}
+        _ -> false
       end)
       |> then(fn {x, y} -> x * @multiplier + y end)
 
@@ -61,24 +48,20 @@ defmodule Day15 do
 
   def outlawed_on_fixed_y_line(sensor_and_beacons, fixed_y) do
     sensor_and_beacons
-    |> Enum.flat_map(fn {{s_x, s_y} = s, b} ->
-      dist_to_nearest_beacon = manhattan_distance(s, b)
-
+    |> Enum.flat_map(fn {{s_x, _} = s, _, dist_to_nearest_beacon} ->
       dist_to_fixed_y_line = manhattan_distance(s, {s_x, fixed_y})
 
-      if dist_to_nearest_beacon >= dist_to_fixed_y_line do
-        remaining_dist = dist_to_nearest_beacon - dist_to_fixed_y_line
+      remaining_dist = dist_to_nearest_beacon - dist_to_fixed_y_line
 
+      if remaining_dist >= 0 do
         [(s_x - remaining_dist)..(s_x + remaining_dist)]
       else
         []
       end
     end)
     |> Enum.sort()
-    |> IO.inspect()
     |> Enum.reduce([], fn
       next, [previous | rest] ->
-        #        IO.inspect({previous, next, rest}, label: "prev next rest")
         merge_adjacent_ranges(previous, next) ++ rest
 
       next, [] ->
@@ -88,20 +71,19 @@ defmodule Day15 do
   end
 
   def merge_adjacent_ranges(previous, next) do
-    # previous and next must be sorted, which implies that previous.first <= next.first
-    cond do
-      # add 1 because ranges are inclusive; 1..4 and 5..8 should combine to 1..8 because the first range includes 4
-      previous.last + 1 >= next.first ->
-        [previous.first..max(previous.last, next.last)]
+    # previous and next must be pre-sorted, which implies that previous.first <= next.first
 
-      previous.last < next.first ->
-        [next, previous]
+    # add 1 because ranges are inclusive; 1..4 and 5..8 should combine to 1..8 because the first range includes 4
+    if previous.last + 1 >= next.first do
+      [previous.first..max(previous.last, next.last)]
+    else
+      [next, previous]
     end
   end
 
   def count_beacons_at_fixed_y(sensors_and_beacons, fixed_y) do
     sensors_and_beacons
-    |> Enum.map(fn {_, b} -> b end)
+    |> Enum.map(fn {_, b, _} -> b end)
     |> MapSet.new()
     |> Enum.filter(fn {_, b_y} -> b_y == fixed_y end)
     |> Enum.count()
