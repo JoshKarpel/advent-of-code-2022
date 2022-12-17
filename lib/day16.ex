79ef8e54e6886f6913b2cb699c43b8d7e58228a5
Josh Karpel <josh.karpel@gmail.com>
2022-12-17 11:45:31 -0600

    parse input

--- lib/day16.ex
+++ lib/day16.ex
@@ -1,8 +1,17 @@
 defmodule Day16 do
   def solve do
-    File.stream!("data/day_16.txt")
-    |> Enum.map(&String.trim/1)
-    |> IO.inspect()
+    valves_flows_tunnels =
+      File.stream!("data/day_16.txt")
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(
+        &Regex.run(~r/Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? ([\w\s,]+)/, &1,
+          capture: :all_but_first
+        )
+      )
+      |> Enum.map(fn [valve, flow, tunnels] ->
+        {valve, String.to_integer(flow), String.split(tunnels, ", ")}
+      end)
+      |> IO.inspect()
 
     {0, 0}
   end
