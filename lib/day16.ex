339ac73eb9af57aace2e217950ec84c403a3c507
Josh Karpel <josh.karpel@gmail.com>
2022-12-17 17:55:12 -0600

    collapse paths, score paths

--- lib/day16.ex
+++ lib/day16.ex
@@ -1,4 +1,6 @@
 defmodule Day16 do
+  @minutes 30
+
   def solve do
     valves_flows_tunnels =
       File.stream!("data/day_16.txt")
@@ -9,10 +11,90 @@ defmodule Day16 do
         )
       )
       |> Enum.map(fn [valve, flow, tunnels] ->
-        {valve, String.to_integer(flow), String.split(tunnels, ", ")}
+        {valve, {String.to_integer(flow), String.split(tunnels, ", ")}}
       end)
-      |> IO.inspect()
+      |> Map.new()
+
+    flows =
+      valves_flows_tunnels
+      |> Enum.map(fn {v, {f, t}} -> {v, f} end)
+      |> Map.new()
+      |> IO.inspect(label: "flows")
+
+    paths = valves_flows_tunnels |> collapse_edges |> IO.inspect(label: "collapsed edges")
+
+    %{"DD" => 2, "BB" => 5, "JJ" => 9, "HH" => 17, "EE" => 21, "CC" => 24}
+    |> score_path(flows)
+    |> IO.inspect(label: "test score")
 
     {0, 0}
+  end
+
+  def score_path(path, flows) do
+    path
+    |> Enum.map(fn {valve, minute} ->
+      Map.get(flows, valve) * (@minutes - minute)
+    end)
+    |> Enum.sum()
+  end
+
+  def collapse_edges(valves_flows_tunnels) do
+    edges = valves_flows_tunnels |> Enum.map(fn {v, {_, t}} -> {v, t} end) |> Map.new()
+
+    has_flow =
+      valves_flows_tunnels
+      |> Enum.map(fn {v, {f, _}} -> {v, f} end)
+      |> Enum.filter(fn {v, f} -> f > 0 or v == "AA" end)
+      |> Enum.map(fn {v, _} -> v end)
+      |> MapSet.new()
+
+    has_flow
+    |> Enum.map(fn v ->
+      {v,
+       shortest_paths(edges, v)
+       |> Enum.filter(fn {target, _} ->
+         Enum.member?(has_flow, target) and v != target
+       end)
+       |> Map.new()}
+    end)
+    |> Map.new()
+  end
+
+  def shortest_paths(edges, start) do
+    _shortest_paths(edges, MapSet.new([start]), %{start => 0}, MapSet.new())
+  end
+
+  def _shortest_paths(edges, frontier, paths, visited) do
+    sorted_frontier =
+      frontier
+      |> Enum.sort_by(fn f -> Map.get(paths, f, @infinity) end)
+
+    case sorted_frontier do
+      [] ->
+        paths
+
+      [head | tail] ->
+        new_visited = visited |> MapSet.put(head)
+        neighbours = Map.get(edges, head, [])
+
+        new_frontier =
+          MapSet.new(tail ++ (neighbours |> Enum.reject(&Enum.member?(new_visited, &1))))
+
+        cost_to_neighbours = Map.get(paths, head) + 1
+
+        new_paths =
+          neighbours
+          |> Enum.reduce(
+            paths,
+            fn n, paths ->
+              paths
+              |> Map.update(n, cost_to_neighbours, fn prev ->
+                Enum.min([prev, cost_to_neighbours])
+              end)
+            end
+          )
+
+        _shortest_paths(edges, new_frontier, new_paths, new_visited)
+    end
   end
 end
