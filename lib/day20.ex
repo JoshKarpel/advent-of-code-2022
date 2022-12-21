5c7e6df5c57a41448536b82c2ca48ec387c5ea0f
Josh Karpel <josh.karpel@gmail.com>
2022-12-20 19:44:25 -0600

    parse input

--- lib/day20.ex
+++ lib/day20.ex
@@ -1,8 +1,15 @@
 defmodule Day20 do
   def solve do
-    File.stream!("data/day_20.txt")
-    |> Enum.map(&String.trim/1)
-    |> IO.inspect()
+    numbers =
+      File.stream!("data/day_20.txt")
+      |> Enum.map(&String.trim/1)
+      |> Enum.map(&String.to_integer/1)
+      |> IO.inspect()
+
+    len =
+      numbers
+      |> Enum.count()
+      |> IO.inspect(label: "total")
 
     {0, 0}
   end
